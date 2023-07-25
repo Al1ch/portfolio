3d05f7e5fdebf4c30a8b94387f2249bf9ba05d21
Alain Chea <alain.chea@efrei.net>
2023-07-25 20:44:03 +0200

    feat(component): wog skills badge

---
+++ components/SkillBadge/SkillBadge.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./SkillBadge.module.scss";
+import { StaticImageData } from "next/image";
+import ReactIcon from "@/assets/vectors/react.svg";
+import Image from "next/image";
+
+type Props = {
+  name: string;
+  url?: string | StaticImageData;
+};
+
+const SkillBadge = ({ name, url }: Props) => {
+  return (
+    <div className={styles.container}>
+      <ReactIcon className={styles.logo} />
+      <span className={styles.infoName}>{name}</span>
+    </div>
+  );
+};
+
+export default SkillBadge;
