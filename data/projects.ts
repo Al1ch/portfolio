bb861bad9fa9fa37b241be45e3be0e507e39f2a8
Alain Chea <alain.chea@efrei.net>
2023-09-25 17:33:52 +0200

    feat(projectsection): add data and color

---
+++ data/projects.ts
@@ -0,0 +1,15 @@
+import projectImage from '@/assets/photo/background_immo.webp'
+
+export const projects = [
+    {
+        id:1,
+        cardInfo:{
+            title: "Project 1",
+            role: "Front End Developer",
+            description : "test"
+        },
+        description: "test",
+        title: "test",
+        image: projectImage,
+    }
+]
